e7fc1834595827b87011ada550d9bcc699af2b77
Student <student@macbook-pro-fiset-algarvio.local>
2017-03-04 15:39:22 -0500

    Made my design with commentes

--- Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -18,20 +18,25 @@
 import Cocoa
 import PlaygroundSupport
 
-// Create canvas
-// let canvas = Canvas(width: 500, height: 500)
-let canvas = Canvas(width: 500, height: 500, quality: Quality.Ultra)
+// The Canvases
+let canvas = Canvas(width: 500, height: 500)
+// let canvas = Canvas(width: 500, height: 500, quality: Quality.Ultra)
 
-// Generate a grid
+// Circles with no fill
 canvas.drawShapesWithFill = false
 canvas.defaultBorderWidth = 2
 
+// Random Generator
+var a = random(from: 0, toButNotIncluding: 361)
+
+// Loops for the circles
 for z in stride(from: 0, through: 100, by: 10) {
     for x in stride(from: 0, through: 400, by: 100) {
         for y in stride(from: 0, through: 400, by: 100) {
             canvas.drawShapesWithBorders = true
-            canvas.drawShapesWithFill = false
-            canvas.borderColor = Color(hue: 120 + z, saturation: 100, brightness: 100, alpha: 100)
+            // The color based on the random generator
+            canvas.borderColor = Color(hue: a + z, saturation: 100, brightness: 100, alpha: 100)
+            // The Circles
             canvas.drawEllipse(centreX: z / 2 + x, centreY: z / 2 + y, width: z, height: z)
             canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
             canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: z / 2 + y, width: z, height: z)
--- Assignment.playground/Pages/Design Two.xcplaygroundpage/Contents.swift
+++ Assignment.playground/Pages/Design Two.xcplaygroundpage/Contents.swift
@@ -21,23 +21,21 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-// Generate a grid
-canvas.drawShapesWithFill = false
-canvas.defaultBorderWidth = 1
-
-// This loop makes a 10 rows of columns
-for x in stride(from: 25, through: 475, by: 50){
-    
-    // This loop makes a single column, bottom to top
-    for y in stride(from: 25, through: 475, by: 50) {
-        
-        // Draw the shapes
-        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
-        canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
+// The loops for the circles
+for z in stride(from: 0, through: 100, by: 10) {
+    for x in stride(from: 0, through: 400, by: 100) {
+        for y in stride(from: 0, through: 400, by: 100) {
+            canvas.drawShapesWithBorders = true
+            // The color starting from 120
+            canvas.borderColor = Color(hue: 120 + z, saturation: 100, brightness: 100, alpha: 100)
+            // The Circles
+            canvas.drawEllipse(centreX: z / 2 + x, centreY: z / 2 + y, width: z, height: z)
+            canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
+            canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: z / 2 + y, width: z, height: z)
+            canvas.drawEllipse(centreX: z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
+        }
     }
 }
-
-
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
